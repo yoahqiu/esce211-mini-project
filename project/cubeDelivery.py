9992349438fbc19934ea0311b44f4008d40ceb08
Walid Aissa <walidaissa047@gmail.com>
2023-03-13 17:00:31 -0400

    changes made to delivery

---
+++ project/cubeDelivery.py
@@ -0,0 +1,37 @@
+from utils.brick import wait_ready_sensors, TouchSensor, Motor, EV3ColorSensor
+from utils.sound import Sound
+from time import sleep
+
+
+from ast import literal_eval
+from math import sqrt, e, pi
+from statistics import mean, stdev
+
+def startDeliveryRoutine():
+    in_delivery_routine = False
+
+
+
+    return in_delivery_routine
+
+def deliver(color):
+
+    if color == "yellow":
+        pass
+
+    if color == "purple":
+        pass
+
+    if color == "orange":
+        pass
+
+    if color == "blue":
+        pass
+
+    if color == "red":
+        pass
+
+    if color == "green":
+        pass
+
+    
--- project/padDetection.py
+++ project/padDetection.py
@@ -7,6 +7,7 @@ from ast import literal_eval
 from math import sqrt, e, pi
 from statistics import mean, stdev
 
+import cubeDelivery as cd
 
 #1. Identify the color mix
 #2. Change left/right steering vector 
@@ -25,6 +26,7 @@ tresholdRed = 0.85
 tresholdGreen = 0.8
 normalDps = 300
 slowDownFactor = 0.55
+in_delivery_routine = False
 
 
 while (True):
@@ -45,29 +47,47 @@ while (True):
     #yellow
     if (r > 0.70 and r < 0.80 and g > 0.60 and g < 0.70 and b > 0.00 and b < 0.10):
         print("yellow")
-        continue
+        if(in_delivery_routine):
+            cd.deliver("yellow")
     
     #purple
     if (r > 0.90 and r < 0.99 and g > 0.10 and g < 0.20 and b > 0.10 and b < 0.20):
         print("purple")
-        continue
+        if(in_delivery_routine):
+            cd.deliver("purple")
     
     #orange
     if (r > 0.90 and r < 0.99 and g > 0.20 and g < 0.30 and b > 0.00 and b < 0.10):
         print("orange")
-        continue
+        if(in_delivery_routine):
+            cd.deliver("orange")
     
     #if detect too much blue, turn left
     elif (b > tresholdBlue):
         print("blue")
 
+
+        #if in delivery position, don't turn but deliver to blue area instead
+        if(in_delivery_routine):
+            cd.deliver("blue")
+
     #if detect too red, turn right
     elif (r > tresholdRed):
         print("red")
 
+        #if in delivery position, don't turn but deliver to red area instead
+        if(in_delivery_routine):
+            cd.deliver("red")
+
     #if detect green, launch cube delivery routine
     elif (g > tresholdGreen):
         print("detect green")
+        in_delivery_routine = cd.startDeliveryRoutine()
+
+
+        #if in delivery position, don't launch cube delivery but deliver to green area instead
+        if(in_delivery_routine):
+            cd.deliver("green")
         
     #if white, go straight
     elif (r > 0.55 and r < 0.80 and g > 0.55 and g < 0.80 and b > 0.20 and b < 0.60):
