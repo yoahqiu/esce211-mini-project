cc29efb020067cc3503ddaf5105f8c274d439582
salimei <salim.benche@gmail.com>
2023-03-16 11:41:48 -0400

    Create stop.py

---
+++ project/stop.py
@@ -0,0 +1,32 @@
+
+from utils.brick import wait_ready_sensors, TouchSensor, Motor, EV3ColorSensor
+from utils.sound import Sound
+from time import sleep
+
+
+from ast import literal_eval
+from math import sqrt, e, pi
+from statistics import mean, stdev
+
+""" Convenience class to stop motors from spinning after carrying out a test.
+"""
+
+#1. Identify the color mix
+#2. Change left/right steering vector 
+#3. Adjust motor speeds
+
+motorL = Motor("C")          # Motor port A (left)
+motorR = Motor("B")          # Motor port B (right)
+colorSensorPath = EV3ColorSensor(1)   # Color sensor 
+
+wait_ready_sensors()
+
+# variables to be calibrated
+tresholdBlue = 0.5
+tresholdRed = 0.85
+tresholdGreen = 0.8
+normalDps = 300
+slowDownFactor = 0.60
+
+motorL.set_dps(0)
+motorR.set_dps(0)
