6d0cbc0421bd077f22a35d2c68cebf142667e31e
salimei <salim.benche@gmail.com>
2023-03-16 12:55:30 -0400

    readjust heading

--- project/pathTrackingService.py
+++ project/pathTrackingService.py
@@ -12,13 +12,13 @@ def adjustHeading(sColor, motorL, motorR):
 
     #if detect too much blue, turn left
     if (sColor == "blue"):
-        motorR.set_dps(motorL.get_dps() * slowDownFactor)
-        motorL.set_dps(normalDps)
+        motorL.set_dps(motorL.get_dps() * slowDownFactor)
+        motorR.set_dps(normalDps)
 
     #if detect too red, turn right
     if (sColor == "red"):
-        motorR.set_dps(normalDps)
         motorL.set_dps(motorR.get_dps() * slowDownFactor)
+        motorR.set_dps(normalDps)
         
     #if white, go straight
     if (sColor == "white"):
