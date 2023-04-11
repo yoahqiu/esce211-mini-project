f43a22380d710c0f5841dae08b662d942f499107
salimei <salim.benche@gmail.com>
2023-04-11 15:02:54 -0400

    forward path improvement
    -robot sticks to boundary between red and white in forward path

--- project/pathTrackingService.py
+++ project/pathTrackingService.py
@@ -45,9 +45,23 @@ def adjustHeading(sColor, motorL, motorR):
         sleep(0.2)
         
     #if white, go straight
-    if (sColor == "white"):
+    if (sColor == "white" and aLineColors[0] == "blue"):
+        dpsRight = motorL.get_dps() * slowDownFactor
+        dpsLeft = normalDps*1.2
+        motorR.set_dps(dpsLeft)
+        motorL.set_dps(dpsRight)
+        #print("adjusting right")
+        sleep(0.2)
+    
+    if (sColor == "white" and aLineColors[0] != "blue"): #in reverse path, travserse normally
+        motorR.set_dps(normalDps)
+        motorL.set_dps(normalDps)
+        sleep(0.2)
+        
+    if (sColor == "green"):
         motorL.set_dps(normalDps)
         motorR.set_dps(normalDps)
+        sleep(0.2)
         
 
         
